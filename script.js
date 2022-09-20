e3f2b42e684779a82ed3f0415f675e350b39f2b2
Tengis0618 <114012170+Tengis0618@users.noreply.github.com>
2022-09-21 01:11:08 +0400

    comment

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ likebtn.addEventListener('click', ()=>{
   input1.value = parseInt(input1.value) +1;
   input1.style.color = "#12ff00";
 });
+//function for the like button - increase by one when clicked
 dislikebtn.addEventListener('click', ()=>{
   input2.value = parseInt(input2.value) +1;
   input2.style.color = "#ff0000";
 });
-
+//function for the dislike button - increase by one when clicked
